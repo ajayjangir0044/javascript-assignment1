e2bdb405d9e630f35c862134814435b2859fe65b
Ajay Jangir <56185219+ajayjangir0044@users.noreply.github.com>
2022-01-28 13:10:07 +0530

    Update swapTwoNumbers.js

--- swapTwoNumbers.js
+++ swapTwoNumbers.js
@@ -1,6 +1,7 @@
 let a = 5;
 let b = 2;
-
+console.log(`The value of a before swapping is ${a}`);
+console.log(`The value of b before swapping is ${b}`);
 [a, b] = [b, a];
-
-console.log(`The value of a after swapping is ${a} and value of b after swapping is ${b}`);
+console.log(`The value of a after swapping is ${a}`);
+console.log(`The value of b after swapping is ${b}`);
